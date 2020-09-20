9f0255ef8060a411cfd23858cb8e4e8227b2acfa
xiecoding <62330651+xiecoding@users.noreply.github.com>
2020-09-20 22:42:09 +0800

    Update 383赎金信.cpp

--- 383赎金信.cpp
+++ 383赎金信.cpp
@@ -1,17 +1,19 @@
-    bool canConstruct(string ransomNote, string magazine) {
-        bool judge=true;
-        int i,j;
-        for(i=0;i<((ransomNote=="")?1:(ransomNote.length()));i++){
-            for(j=0;j<((magazine=="")?1:(magazine.length()));j++){
-                if(ransomNote[i]==magazine[j]){
-                    magazine[j]=' ';
-                    break;
-                }
-            }
-            if(j>((magazine=="")?1:(magazine.length()-1))){
-                judge=false;
+//https://leetcode-cn.com/problems/ransom-note/
+
+bool canConstruct(string ransomNote, string magazine) {
+    bool judge=true;
+    int i,j;
+    for(i=0;i<((ransomNote=="")?1:(ransomNote.length()));i++){
+        for(j=0;j<((magazine=="")?1:(magazine.length()));j++){
+            if(ransomNote[i]==magazine[j]){
+                magazine[j]=' ';
                 break;
             }
         }
-        return judge;
+        if(j>((magazine=="")?1:(magazine.length()-1))){
+            judge=false;
+            break;
+        }
     }
+    return judge;
+}
